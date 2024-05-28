c0e49a7e1eca4694e94937a73c637908d3fd1492
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2024-05-29 00:50:50 +0800

    format

--- autoboot-patcher.sh
+++ autoboot-patcher.sh
@@ -5,15 +5,15 @@ MAGISK_BOOT=$1
 export PATH=bin:$PATH
 
 if [ $1 ]; then
-    echo "--- use $1 as magisk patched boot image"
+  echo "--- use $1 as magisk patched boot image"
 else
-    echo "Failed!!! - Please choose magisk patched boot image"
-    echo "Usage:"
-    echo "       ./autoboot-patcher.sh <magisk_patched_boot.img>"
-    exit 1
+  echo "Failed!!! - Please choose magisk patched boot image"
+  echo "Usage:"
+  echo "       ./autoboot-patcher.sh <magisk_patched_boot.img>"
+  exit 1
 fi
 
-#clean
+# clean
 echo "--- Cleaning..."
 magiskboot cleanup
 
@@ -21,18 +21,18 @@ magiskboot cleanup
 echo "--- Unpacking img..."
 magiskboot unpack $MAGISK_BOOT
 
-#ramdisk patch
+# ramdisk patch
 echo "--- Patching ramdisk..."
 magiskboot cpio ramdisk.cpio \
-"mkdir 0700 overlay.d" \
-"add 0700 overlay.d/init.custom.rc init.custom.rc" 
+  "mkdir 0700 overlay.d" \
+  "add 0700 overlay.d/init.custom.rc init.custom.rc"
 echo "--- Packing img..."
 magiskboot repack $MAGISK_BOOT
 
 if [ -f new-boot.img ]; then
-    echo "### new-boot.img --- created"
-    echo "### use \"fastboot flash boot new-boot.img\" to flash boot image to devices"
+  echo "### new-boot.img --- created"
+  echo "### use \"fastboot flash boot new-boot.img\" to flash boot image to devices"
 else
-    echo "### ERROR!!! --- patch failed"
-    exit 1
+  echo "### ERROR!!! --- patch failed"
+  exit 1
 fi
